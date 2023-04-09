22487498c3a371f02bd2750f98c3f903ec47ffd7
dmardanova <115954310+dmardanova@users.noreply.github.com>
2023-04-10 01:16:48 +0300

    Add files via upload

---
+++ курсовая111.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include "Header1.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUSSIAN");
+	cout << "\tКурсовая работа:\n";
+	cout << "Практическая работа номер 1\n";
+	cout << "Практическая работа номер 2\n";
+	cout << "Практическая работа номер 3\n";
+	cout << "Практическая работа номер 4\n";
+	int number;
+	cin >> number;
+	cin.sync();
+	switch (number) {
+	case 1:
+		system("cls");
+		p11();
+		break;
+	case 2:
+		p22();
+		break;
+	case 3:
+		p33();
+		break;
+	case 4:
+		p44();
+		break;
+	}
+	main();
+	return 0;
+}
+
+
